5dcbe50683d08344550323f295da6a099ffc483d
josue173 <josue.perez04@outlook.com>
2023-06-27 16:08:39 -0600

    Agregando Pipes globales

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,6 +1,12 @@
 /* eslint-disable prettier/prettier */
+
+import { IsString, MinLength } from 'class-validator';
+
 // Es una clase y no una interfaz porque se harán validaciones de la información, eso no es posible con las intefaces
 export class CreateCarDto {
+  @IsString() //Decorador para la metadata
   readonly brand: string;
+  @IsString()
+  @MinLength(3) // Validaciones
   readonly model: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); // Esto crea la aplicación de Nest
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Solo deja "entrar" la información especificada en los DTOs
+      forbidNonWhitelisted: true, // Da un error cuando "entra" información no especificada en los DTOs
+    }),
+  );
   await app.listen(3000); // Puerto en el que se levanta la aplicación
 }
 bootstrap();
